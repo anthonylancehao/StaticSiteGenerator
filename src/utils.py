02addbdbc39af2d6dbf67091c347e6542ab5c7b9
Anthony Lance Hao <anthonylancehao@Anthonys-MacBook-Pro.local>
2025-06-03 15:31:34 +0800

    Fix all internal href/src paths using basepath

--- src/utils.py
+++ src/utils.py
@@ -8,17 +8,21 @@ def generate_page(content_path, template_path, output_path, basepath="/"):
 
     html_content = markdown.markdown(markdown_content)
 
-    # Inject basepath into href/src if not absolute
-    def add_basepath(match):
+    # Fix href/src inside the generated HTML (from markdown)
+    def fix_paths(match):
         attr, url = match.groups()
         if url.startswith(("http://", "https://", "mailto:", "#", basepath)):
             return f'{attr}="{url}"'
+        # Otherwise, prefix with basepath
         return f'{attr}="{basepath.rstrip("/")}/{url.lstrip("/")}"'
 
-    html_content = re.sub(r'(href|src)="([^"]+)"', add_basepath, html_content)
+    html_content = re.sub(r'(href|src)="([^"]+)"', fix_paths, html_content)
 
     with open(template_path, "r") as f:
         template = f.read()
+
+    # Also fix href/src in the template
+    template = re.sub(r'(href|src)="([^"]+)"', fix_paths, template)
 
     full_html = template.replace("{{ content }}", html_content)
 
